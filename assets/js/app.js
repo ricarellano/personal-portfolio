40eac29791260d95d43e85d47046605f32e3d25d
ricarellano <rarellanob@gmail.com>
2017-03-07 18:48:07 -0800

    add second project to portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,11 @@
 console.log("app.js linked.");
 
 $(document).ready(function() {
-  $('#myModal').on('shown.bs.modal', function () {
+  $('#myModal').on('#modal2', function () {
  $('#myInput').focus()
+
+ $('#myModal').on('#modal1', function () {
+$('#myInput').focus()
 })
 
 
